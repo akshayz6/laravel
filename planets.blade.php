3f9473a46195f7288dc52d458c7fe309ce75e983
akshayz6 <133095697+akshayz6@users.noreply.github.com>
2024-06-28 20:21:08 +0200

    Add files via upload

--- planets.blade.php
+++ planets.blade.php
@@ -17,3 +17,24 @@
     </ul>
 </body>
 </html>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Planets List</title>
+</head>
+<body>
+    <h1>Planets</h1>
+    <ul>
+        @forelse ($planets as $planet)
+            <li>
+                <h2>{{ $planet['name'] }}</h2>
+                <p>{{ $planet['description'] }}</p>
+            </li>
+        @empty
+            <li>No planets found.</li>
+        @endforelse
+    </ul>
+</body>
+</html>
--- web.php
+++ web.php
@@ -32,3 +32,38 @@ Route::get('/planets', function () {
     
     return view('planets', ['planets' => $planets]);
 });
+
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/planets', function () {
+    $planets = collect([
+        [
+            'name' => 'Mars',
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+        ],
+        [
+            'name' => 'Venus',
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+        ],
+        [
+            'name' => 'Earth',
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
+        ],
+        [
+            'name' => 'Jupiter',
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+        ],
+    ]);
+
+    $planetName = request('planet');
+
+    if ($planetName) {
+        $planets = $planets->filter(function ($planet) use ($planetName) {
+            return strtolower($planet['name']) === strtolower($planetName);
+        });
+    }
+
+    return view('planets', ['planets' => $planets->values()->all()]);
+});
