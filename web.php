eeafd46c59b8345b74af22224bbac2d2b64bc20e
akshayz6 <133095697+akshayz6@users.noreply.github.com>
2024-06-28 20:14:59 +0200

    Add files via upload

---
+++ web.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/planeten', function () {
+    $planeten = ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
+    return response()->json($planeten);
+});
